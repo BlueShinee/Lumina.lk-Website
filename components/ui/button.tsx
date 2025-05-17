f1ce1202508a8fcc76f22f4c71e680d6f3afe0c0
7A7z <sadaminatou@gmail.com>
2025-05-18 02:45:37 +0530

    Supporting mods for the mcq cards

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,9 +20,12 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        mcq: "bg-primary/10 text-primary hover:text-primary-foreground shadow-none hover:bg-primary/70 w-full justify-start text-left",
+        mcqSelected:
+          "bg-primary text-primary-foreground shadow-none hover:bg-primary/90 w-full justify-start text-left",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        default: "min-h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
@@ -33,7 +36,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 function Button({
   className,
