faeeaa269543cc131822daed3001754ea0b4a984
7A7z <sadaminatou@gmail.com>
2025-05-18 02:45:48 +0530

    Removed the background frmo the suspense loading state

--- app/mcqv2/[params]/page.tsx
+++ app/mcqv2/[params]/page.tsx
@@ -10,16 +10,18 @@ export default async function page({
   let [qCatagory, qAmount] = params_result.params.split("-");
 
   return (
-    <div className="w-full min-h-screen flex flex-col">
-      <Suspense
-        fallback={
-          <div className="w-full flex h-screen justify-center items-center bg-zinc-50">
-            <span className="loading loading-spinner loading-xl text-orange-600"></span>
-          </div>
-        }
-      >
-        <Mcqs qCatagory={qCatagory} qAmount={qAmount} />
-      </Suspense>
+    <div className="w-full min-h-screen flex flex-col items-center">
+      <div className="w-[50%] mt-16 flex flex-col">
+        <Suspense
+          fallback={
+            <div className="w-full flex h-screen justify-center items-center">
+              <span className="loading loading-spinner loading-xl text-orange-600"></span>
+            </div>
+          }
+        >
+          <Mcqs qCatagory={qCatagory} qAmount={qAmount} />
+        </Suspense>
+      </div>
     </div>
   );
 }
