f7714d80d77dbb279cffa27b8362ae49dea45a96
7A7z <sadaminatou@gmail.com>
2025-05-18 01:50:55 +0530

    mcq algorithm update

--- app/mcqv2/[params]/mcqGenAlgo.ts
+++ app/mcqv2/[params]/mcqGenAlgo.ts
@@ -4,16 +4,30 @@ import { getMcqQuestionsByCategoryId } from "@/database";
 
 export async function genarateQuestions(catagory: number, amount: number) {
   let qAll = await getMcqQuestionsByCategoryId(catagory);
+
+  // Random question genarater
+  //-------------------------------------------------------------
   let qSelected: typeof qAll = [];
-  qAll.map((v, i) => {
-    if (qSelected.length < amount) {
-      let heat = Math.floor(Math.random() * 10);
-      if (heat > 5) {
-        qSelected.push(v);
+  let loop: boolean = true;
+  let counter: number = 0;
+  while (loop) {
+    let heat = Math.floor(Math.random() * 10);
+    if (heat > 8) {
+      if (!qSelected.includes(qAll[counter])) {
+        qSelected.push(qAll[counter]);
       }
-    } else {
-      return qSelected;
     }
-  });
-    return qSelected;
+
+    if (counter >= qAll.length) {
+      counter = 0;
+    }
+    if (qSelected.length >= amount) {
+      loop = false;
+      }
+    counter++;
+  }
+  //----------------------------------------------------
+
+  console.log(qSelected);
+  return qSelected;
 }
